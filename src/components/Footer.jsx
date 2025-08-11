8305fb2d9898175e0b43ce87a32d76b414318800
codebyaddro <codebyaddro@gmail.com>
2025-08-12 03:38:12 +0600

    add(footer): footer menu added
    active menu and hover menu design are not add yet

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import "../assets/css/components/Footer.css"
 
 const Footer = () => {
     return (
-        <div>
-            This is a footer
-        </div>
+        <>
+            <div className="cba-footer-container">
+                <div className="cba-fc-menu-box">
+                    <div className="cba-fc-mb-menu">
+                        <img src="../../public/images/games-icon.png" alt="" />
+                        <h4>Games</h4>
+                    </div>
+                    <div className="cba-fc-mb-menu">
+                        <img src="../../public/images/my-matches-icon.png" alt="" />
+                        <h4>My Matches</h4>
+                    </div>
+                    <div className="cba-fc-mb-menu">
+                        <img src="../../public/images/results-icon.png" alt="" />
+                        <h4>Results</h4>
+                    </div>
+                    <div className="cba-fc-mb-menu">
+                        <img src="../../public/images/profile-icon.png" alt="" />
+                        <h4>Profile</h4>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
