826d88bc636ce95603dc4f7f3f4994e85249ba02
codebyaddro <codebyaddro@gmail.com>
2025-08-16 04:36:39 +0600

    game page heading and carousel done

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -4,16 +4,19 @@ const Game = () => {
     return (
         <>
             <div className="cba-game-page-container">
+                {/* game page main content box */}
                 <div className="cba-gpc-content">
+                    {/* game page heading box */}
                     <div className="cba-gpc-heading">
                         <div className="cba-gpc-hbox">
                             <h2>
-                                <i class='bx  bxs-fire-alt'  ></i>
+                                <i class='bx  bxs-fire-alt'></i>
                                 OFFER NOTICE BOARD
-                                <i class='bx  bxs-fire-alt'  ></i>
+                                <i class='bx  bxs-fire-alt'></i>
                             </h2>
                         </div>
                     </div>
+                    {/* game page carousel div */}
                     <div className="cba-gpc-carousel">
                         <div className="cba-gpc-carousel-slider">
                             <img src="../../public/images/carousel-img-one.png" alt="Carousel Image 1" />
@@ -22,6 +25,7 @@ const Game = () => {
                             <img src="../../public/images/carousel-img-four.png" alt="Carousel Image 4" />
                         </div>
                     </div>
+                    {/* game page todays match box */}
                 </div>
             </div>
         </>
