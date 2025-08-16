97f67e2d3cc34a22c30ffb326aae9f31f02644aa
codebyaddro <codebyaddro@gmail.com>
2025-08-16 12:09:10 +0600

    todays match box done

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -26,6 +26,87 @@ const Game = () => {
                         </div>
                     </div>
                     {/* game page todays match box */}
+                    <div className="cba-gpc-todays-match">
+                        {/* game page todays match heading */}
+                        <div className="cba-gpc-tm-heading">
+                            <div className="cba-gpc-tm-hbox">
+                                <h2>
+                                    <i class='bx  bxs-fire-alt'></i>
+                                    TODAY’S ALL MATCHES
+                                    <i class='bx  bxs-fire-alt'></i>
+                                </h2>
+                            </div>
+                        </div>
+                        {/* game page todays match content matches */}
+                        <div className="cba-gpc-tm-matches">
+                            <div className="cba-gpc-tmm-match-box">
+                                <div className="cba-gpc-tmm-mb-img">
+                                    <img src="../../public/images/br-match.png" alt="BR Match" />
+                                </div>
+                                <div className="cba-gpc-tmm-mb-data">
+                                    <div className="cba-gpc-tmm-mbd-inner">
+                                        <h3>BR Matches</h3>
+                                        <p>10 Match Found</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="cba-gpc-tmm-match-box">
+                                <div className="cba-gpc-tmm-mb-img">
+                                    <img src="../../public/images/c-squad-match.png" alt="Class Squad Match" />
+                                </div>
+                                <div className="cba-gpc-tmm-mb-data">
+                                    <div className="cba-gpc-tmm-mbd-inner">
+                                        <h3>Class Squad</h3>
+                                        <p>10 Match Found</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="cba-gpc-tmm-match-box">
+                                <div className="cba-gpc-tmm-mb-img">
+                                    <img src="../../public/images/cs-match.png" alt="CS 1V1 & 2V2 Match" />
+                                </div>
+                                <div className="cba-gpc-tmm-mb-data">
+                                    <div className="cba-gpc-tmm-mbd-inner">
+                                        <h3>CS 1V1 & 2V2</h3>
+                                        <p>10 Match Found</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="cba-gpc-tmm-match-box">
+                                <div className="cba-gpc-tmm-mb-img">
+                                    <img src="../../public/images/lw-match.png" alt="Lone Wolf Match" />
+                                </div>
+                                <div className="cba-gpc-tmm-mb-data">
+                                    <div className="cba-gpc-tmm-mbd-inner">
+                                        <h3>Lone Wolf</h3>
+                                        <p>10 Match Found</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="cba-gpc-tmm-match-box">
+                                <div className="cba-gpc-tmm-mb-img">
+                                    <img src="../../public/images/free-match.png" alt="Free Match" />
+                                </div>
+                                <div className="cba-gpc-tmm-mb-data">
+                                    <div className="cba-gpc-tmm-mbd-inner">
+                                        <h3>Free Match</h3>
+                                        <p>10 Match Found</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="cba-gpc-tmm-match-box">
+                                <div className="cba-gpc-tmm-mb-img">
+                                    <img src="../../public/images/cc-match.png" alt="Custom Challenge Match" />
+                                </div>
+                                <div className="cba-gpc-tmm-mb-data">
+                                    <div className="cba-gpc-tmm-mbd-inner">
+                                        <h3>BR Match</h3>
+                                        <p>10 Match Found</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
